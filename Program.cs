1cf54a9c78eabe3e5f0d6859452094b3ffd9a9c7
adavletyarov <adavletyarov@fors.ru>
2020-08-17 16:13:16 +0300

    Решение

---
+++ Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Discount
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            // исходный товар
+            var item = new Item("Шапка", 1.25);
+
+            // на витрине 3 позиции
+            var showcase = new List<ISellableItem>();
+            ISellableItem baseItem = new BaseItem(item);
+            showcase.Add(baseItem);
+            showcase.Add(new DiscountedItem(baseItem, 0));
+            showcase.Add(new DiscountedItem(baseItem, 10));
+            PrintShowcase(showcase);
+
+            // меняем цену товара
+            item.Price = 10.35f;
+            PrintShowcase(showcase);
+
+            // меняем способ доставки
+            baseItem.Delivery = new Pickup(5);
+            PrintShowcase(showcase);
+
+            baseItem.Delivery = new Courier(25);
+            PrintShowcase(showcase);
+
+            //Присваиваем неверный тип доставки для товара со скидкой - ошибка
+            //item11.Delivery = new Courier(15);
+
+            Console.Read();
+        }
+
+        private static void PrintShowcase(IEnumerable<ISellableItem> showcase)
+        {
+            foreach (var item in showcase)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---");
+        }
+    }
+
+    public abstract class Delivery
+    {
+        public double Price { get; }
+
+        protected Delivery(double price)
+        {
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Price:0.00}";
+        }
+    }
+
+    public class Pickup : Delivery
+    {
+        private const double DefaultPrice = 10;
+
+        public Pickup(double price) : base(price) { }
+        public Pickup() : base(DefaultPrice) { }
+
+        public override string ToString()
+        {
+            return base.ToString() + " (самовывоз)";
+        }
+    }
+
+    public class Courier : Delivery
+    {
+        private const double DefaultPrice = 50;
+
+        public Courier(double price) : base(price) { }
+        public Courier() : base(DefaultPrice) { }
+
+        public override string ToString()
+        {
+            return base.ToString() + " (курьером)";
+        }
+    }
+
+    public class Item
+    {
+        public readonly string Name;
+        public double Price;
+
+        public Item(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+
+    public interface ISellableItem
+    {
+        Item Item { get; }
+        Delivery Delivery { get; set; }
+        double TotalPrice { get; }
+    }
+
+    public class BaseItem : ISellableItem
+    {
+        public Item Item { get; }
+        public Delivery Delivery { get; set; }
+        public double TotalPrice => Item.Price + Delivery.Price;
+
+        public BaseItem(Item item)
+        {
+            Item = item;
+            Delivery = new Courier();
+        }
+
+        public override string ToString()
+        {
+            return $"Наименование: {Item.Name}" +
+                   $", цена: {Item.Price:0.00}" +
+                   $", доставка: {Delivery}" +
+                   $", итого: {TotalPrice:0.00}";
+        }
+    }
+
+    public class DiscountedItem : ISellableItem
+    {
+        private readonly ISellableItem _item;
+        private readonly int _discount;
+        private Delivery _delivery;
+
+        public Item Item  => _item.Item;
+        public Delivery Delivery
+        {
+            get => _delivery;
+            set
+            {
+                if (!(value is Pickup))
+                {
+                    throw new ArgumentException("Недопустимый тип доставки");
+                }
+                _delivery = value;
+            }
+        }
+        public double TotalPrice => Item.Price - GetDiscount() + Delivery.Price;
+
+        public DiscountedItem(ISellableItem item, int discount)
+        {
+            _item = item;
+            _discount = discount;
+            _delivery = new Pickup();
+        }
+
+        private double GetDiscount()
+        {
+            return Item.Price * _discount / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"Наименование: {Item.Name}" +
+                   $", цена: {Item.Price:0.00}" +
+                   $", скидка: {GetDiscount():0.00} ({_discount}%)" +
+                   $", доставка: {_delivery}" +
+                   $", итого: {TotalPrice:0.00}";
+        }
+    }
+}
